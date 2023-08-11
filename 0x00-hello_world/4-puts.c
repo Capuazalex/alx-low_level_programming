1cafe3b43b14a2db3ade4fc525b47ac09a471c1f
Capuazalex <rotimiakinlade20@gmail.com>
2023-08-10 18:33:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a new line.
+ * puzzle
  * Return: Always 0.
  */
 
@@ -13,5 +13,4 @@ int main(void)
 	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
-}	
-		
+}			
