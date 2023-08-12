f5de7ed014edb6eee8874912f51f0a1205062c1c
Capuazalex <rotimiakinlade20@gmail.com>
2023-08-12 10:48:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints "with proper grammer, but the outcome is a piece of art," followed newline
- * Return: Always 0
- *
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
  */
-
 int main(void)
-
-{	
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
-}	
+{
+print("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
