a616e8018399b04353f7255e5f970cb167e467f2
Capuazalex <rotimiakinlade20@gmail.com>
2023-09-01 10:05:26 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array that contains the program commamd line arguments
  * Return: (0) when successful
  */
-int main(int argc, char **argv __attribute__ ((unused))
+int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
