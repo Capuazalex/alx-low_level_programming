240299bef1bac89c6d75e8b98cf23050b937ffc2
Capuazalex <rotimiakinlade20@gmail.com>
2023-09-05 03:11:39 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -32,7 +32,7 @@ char **strtow(char *str)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 		height++;
 	{
-		aout = malloc((heigt + 1) * sizeof(char *));
+		aout = malloc((height + 1) * sizeof(char *));
 	}
 		if (aout == NULL)
 		{
